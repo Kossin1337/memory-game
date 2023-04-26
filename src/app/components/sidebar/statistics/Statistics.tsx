e7b03ba0d1ec8f551728c79b29324e5a56a495dc
Kossin1337 <piotrswobodzinski@gmail.com>
2023-04-26 20:58:06 +0200

    Sidebar statistics styling

--- src/app/components/sidebar/statistics/Statistics.tsx
+++ src/app/components/sidebar/statistics/Statistics.tsx
@@ -22,29 +22,89 @@ const Statistics = () => {
   return (
     <div className="statistics-container">
       <div className="stat-box">
-        <h4>Total Games</h4>
-        <span>137</span>
-      </div>
-      <div className="stat-box">
-        <h4>AVG TURNS</h4>
-        <span>6.9</span>
+        <span className="total-games-text">Total Games</span>
+        <span className="total-games-text">137</span>
       </div>
       <div className="difficulties">
         <div className="difficulty easy">
-          <span className="difficulty-title easy">EASY</span>
-          <span className="difficulty-turns">3x4</span>
+          <div className="difficulty-header">
+            <span className="difficulty-title">EASY</span>
+            <span className="difficulty-turns">3x4</span>
+          </div>
+          <div className="info">
+            <div className="info-box games">
+              <span className="name">Games</span>
+              <span className="value">XXX</span>
+            </div>
+            <div className="info-box turns">
+              <span className="name">Turns</span>
+              <span className="value">XXX</span>
+            </div>
+            <div className="info-box time">
+              <span className="name">Time</span>
+              <span className="value">XXX</span>
+            </div>
+          </div>
         </div>
         <div className="difficulty casual">
-          <span className="difficulty-title">MEDIUM</span>
-          <span className="difficulty-turns">4x5</span>
+          <div className="difficulty-header">
+            <span className="difficulty-title">CASUAL</span>
+            <span className="difficulty-turns">4x5</span>
+          </div>
+          <div className="info">
+            <div className="info-box games">
+              <span className="name">Games</span>
+              <span className="value">XXX</span>
+            </div>
+            <div className="info-box turns">
+              <span className="name">Turns</span>
+              <span className="value">XXX</span>
+            </div>
+            <div className="info-box time">
+              <span className="name">Time</span>
+              <span className="value">XXX</span>
+            </div>
+          </div>
         </div>
         <div className="difficulty hard">
-          <span className="difficulty-title ">HARD</span>
-          <span className="difficulty-turns">5x6</span>
+          <div className="difficulty-header">
+            <span className="difficulty-title">HARD</span>
+            <span className="difficulty-turns">5x6</span>
+          </div>
+          <div className="info">
+            <div className="info-box games">
+              <span className="name">Games</span>
+              <span className="value">XXX</span>
+            </div>
+            <div className="info-box turns">
+              <span className="name">Turns</span>
+              <span className="value">XXX</span>
+            </div>
+            <div className="info-box time">
+              <span className="name">Time</span>
+              <span className="value">XXX</span>
+            </div>
+          </div>
         </div>
         <div className="difficulty ultra">
-          <span className="difficulty-title">ULTRA</span>
-          <span className="difficulty-turns">6x7</span>
+          <div className="difficulty-header">
+            <span className="difficulty-title">ULTRA</span>
+            <span className="difficulty-turns">6x7</span>
+          </div>
+          <div className="info">
+            <div className="info-box games">
+              <span className="name">Games</span>
+              <span className="value">XXX</span>
+            </div>
+            <div className="info-box turns">
+              <span className="name">Turns</span>
+              <span className="value">XXX</span>
+            </div>
+            <div className="info-box time">
+              <span className="name">Time</span>
+              <span className="value">XXX</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
